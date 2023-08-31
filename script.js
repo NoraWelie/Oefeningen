c753fefac030733ae402b900f5c0c82e0c063a5f
NoraWelie <113632553+NoraWelie@users.noreply.github.com>
2023-08-31 10:48:55 +0000

    opdracht 12

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-var horizontaalA = 120;
-var horizontaalB = 500;
-var verticaal = 170;
+var xJOS = 350;
+var yJOS = 350;
 
 function setup() {
-  canvas = createCanvas(1000,300);
+  canvas = createCanvas(450,450);
   canvas.parent();
   textFont("Verdana");
-  textSize(30);
-  stroke('white');
-  strokeWeight(10);
-  frameRate(50);
+  textSize(14);
 }
 
 function draw() {
-  background('orange');
+  background('lavender');
+  fill('black');
+  text("mouseX:" + round(mouseX) + " mouseY:" + round(mouseY),10,20);
+  
+  // text("xJOS:" + xJOS + " yJOS:" + yJOS,300,20);
+  
+  translate(mouseX,mouseY);
+  
+  // in de volgende regels wordt JOS getekend
+
+  push();
+  noStroke();
+  fill('indianred');
+  ellipse(0,0,150);
+  fill('slategray');
+  ellipse(-20,-30,50);
+  ellipse(20,-30,50);
   fill('white');
-  rect(0,0,width,40);
-  fill('black');  
-  text("positie A = " + horizontaalA + " positie B = " + horizontaalB,10,30);
-  fill('dodgerblue');
-  ellipse(horizontaalA,verticaal,200);
-  fill('darkred');
-  ellipse(horizontaalB,verticaal,200);
-  horizontaalA += 2;
-  horizontaalB += 1;
+  ellipse(-20,-25,20,40);
+  ellipse(20,-25,20,40);
+  fill('orange');
+  ellipse(0,10,50);
+  stroke('slategray');
+  strokeWeight(10);
+  fill('white');
+  arc(0, 40, 80, 40, 0, PI, CHORD);
+  pop();
+  // einde tekenen JOS
+  
+  // xJOS--;
+  // yJOS -= 2;
+  
 }
