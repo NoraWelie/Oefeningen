d174c633edf1e53de6edf1894cbf10796dc3f9c1
NoraWelie <113632553+NoraWelie@users.noreply.github.com>
2023-09-01 12:35:11 +0000

    opdracht 24

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
-var x = 50;
-var y = 50;
+var xJager = 50;
+var yJager = 50;
+var xProoi = 800;
+var yProoi = 175;
 
 function setup() {
   canvas = createCanvas(1000,400);
   canvas.parent();
   textFont("Verdana");
-  textSize(14);
+  textSize(140);
   noStroke();
   frameRate(50);
 }
 
 function draw() {
   background('olive');
-  
   if (keyIsDown(LEFT_ARROW)) {
-    x -= 5;
+    xJager -= 5;
   }
   if (keyIsDown(RIGHT_ARROW)) {
-    x += 5;
+    xJager += 5;
   }
   if (keyIsDown(UP_ARROW)) {
-    y -= 5;
+    yJager -= 5;
   }
   if (keyIsDown(DOWN_ARROW)) {
-    y += 5;
+    yJager += 5;
   }
-  x = constrain(x,0,width - 100);
-  y = constrain(y,0,height - 100);
+  if (keyIsDown(65)) {
+    xProoi -= 5;
+  }
+  if (keyIsDown(68)) {
+    xProoi += 5;
+  }
+  if (keyIsDown(87)) {
+    yProoi -= 5;
+  }
+  if (keyIsDown(83)) {
+    yProoi += 5;
+  }
 
-  if (x >= 700 && x <= 875 && y >= 75 && y <= 225) {
+  xJager = constrain(xJager,0,width - 100);
+  yJager = constrain(yJager,0,height - 100);
+  xProoi = constrain(xProoi,0,width - 75);
+  yProoi = constrain(yProoi,0,height - 50);
+
+ if (xJager >= xProoi-100 && xJager <= xProoi + 75 && 
+ yJager >= yProoi - 100 && yJager <= yProoi + 50) {
     fill('chartreuse');
+    eindScherm();
   }
   else {
     fill('darkkhaki');
   }
-  
-  rect(800,175,75,50);
-  
+  rect(xProoi,yProoi,75,50);
   fill('moccasin');
-  rect(x,y,100,100);   
-}
+  rect(xJager,yJager,100,100);   
+ }
+
+ function eindScherm() {
+  background('white');
+  fill('black');
+  text("GEVANGEN!",75,250);
+  noLoop();
+ }
