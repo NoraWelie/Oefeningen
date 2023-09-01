e46f1703671a15ee3a9c601b346f27d955c9496f
NoraWelie <113632553+NoraWelie@users.noreply.github.com>
2023-09-01 12:44:30 +0000

    opdracht 27

--- script.js
+++ script.js
@@ -1,65 +1,56 @@
-var xJager = 50;
-var yJager = 50;
-var xProoi = 800;
-var yProoi = 175;
+var aantal = 4;
+var breedte;
 
 function setup() {
-  canvas = createCanvas(1000,400);
+  canvas = createCanvas(1000,300);
   canvas.parent();
-  textFont("Verdana");
-  textSize(140);
   noStroke();
-  frameRate(50);
+  textFont("Verdana");
+  textSize(14);
+  frameRate(5);
 }
 
 function draw() {
-  background('olive');
-  if (keyIsDown(LEFT_ARROW)) {
-    xJager -= 5;
+  background('lavender');
+  fill('black');
+  breedte = width / aantal;
+  text("aantal = " + aantal,10,20);  
+  
+  push();
+  translate(0.5*breedte,150);
+
+  for (var n = 0; n < aantal;n++) {
+    tekenJos(2 + n / 2);
+    translate(breedte,0);
+  }
+  
+  pop();
+  
+  if (keyIsDown(LEFT_ARROW) && aantal >= 2) {
+    aantal--;
   }
   if (keyIsDown(RIGHT_ARROW)) {
-    xJager += 5;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    yJager -= 5;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    yJager += 5;
-  }
-  if (keyIsDown(65)) {
-    xProoi -= 5;
-  }
-  if (keyIsDown(68)) {
-    xProoi += 5;
-  }
-  if (keyIsDown(87)) {
-    yProoi -= 5;
-  }
-  if (keyIsDown(83)) {
-    yProoi += 5;
-  }
+    aantal++;
+  }   
+}
 
-  xJager = constrain(xJager,0,width - 100);
-  yJager = constrain(yJager,0,height - 100);
-  xProoi = constrain(xProoi,0,width - 75);
-  yProoi = constrain(yProoi,0,height - 50);
-
- if (xJager >= xProoi-100 && xJager <= xProoi + 75 && 
- yJager >= yProoi - 100 && yJager <= yProoi + 50) {
-    fill('chartreuse');
-    eindScherm();
-  }
-  else {
-    fill('darkkhaki');
-  }
-  rect(xProoi,yProoi,75,50);
-  fill('moccasin');
-  rect(xJager,yJager,100,100);   
- }
-
- function eindScherm() {
-  background('white');
-  fill('black');
-  text("GEVANGEN!",75,250);
-  noLoop();
- }
+function tekenJos(s) {
+  push();
+  scale(s);
+  noStroke();
+  fill('indianred');
+  ellipse(0,0,50);
+  fill('slategray');
+  ellipse(-7,-10,17);
+  ellipse(7,-10,17);
+  fill('white');
+  ellipse(-7,-8,7,13);
+  ellipse(7,-8,7,13);
+  fill('orange');
+  ellipse(0,3,17);
+  stroke('slategray');
+  strokeWeight(3);
+  fill('white');
+  arc(0, 13, 26, 13, 0, PI, CHORD);
+  pop();
+}
