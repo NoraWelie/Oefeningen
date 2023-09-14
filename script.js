319479a19764f68778a6fd54e1515ac9c784136c
NoraWelie <113632553+NoraWelie@users.noreply.github.com>
2023-09-14 10:39:02 +0000

    2.2 opdracht 10

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-var aantalRijenRaster = 6;
-var aantalKolommenRaster = 9;
-var celGrootte;
-
-var spriteJos;
-var xJos = 400;
-var yJos = 300;
+var animatie = [];
+var aantalBeeldjes = 3;
+var nummer = 0;
 
 function preload() {
-  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
-  spriteJos = loadImage("images/sprites/Jos100px/Jos_0.png");
+  for (var b = 0;b < aantalBeeldjes;b++) {
+    nieuw_beeldje = loadImage("images/sprites/Jos_losse_beeldjes/Jos-" + b +".png");
+    animatie.push(nieuw_beeldje);
+  }
 }
 
 function setup() {
-  canvas = createCanvas(900,600);
+  canvas = createCanvas(460,460);
   canvas.parent();
-  celGrootte = width / aantalKolommenRaster;
+  noStroke();
+  frameRate(2);
+  textFont("Georgia");
+  textSize(18);
 }
 
 function draw() {
-  background(brug);
-  tekenRaster();
-  image(spriteJos,xJos,yJos);
-}
+  background('lavender');
+  image(animatie[nummer],80,160,300,300);
+  nummer++;
 
-function tekenRaster() {
-  push();
-  noFill();
-  stroke('grey');
-  for (var rij = 0;rij < aantalRijenRaster;rij++) {
-    for (var kolom = 0;kolom < aantalKolommenRaster;kolom++) {
-      rect(kolom*celGrootte,rij*celGrootte,celGrootte,celGrootte);
-    }
+  if (nummer == aantalBeeldjes) {
+    nummer = 0;
   }
-  pop();
+  
+  text("frameCount=" + frameCount,5,20);
+  text("nummer=" + nummer,5,40);
 }
