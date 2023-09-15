f536ae4b6b7cc8450eeb22cb4de3d463a0cd2a34
NoraWelie <113632553+NoraWelie@users.noreply.github.com>
2023-09-15 11:45:56 +0000

    2.4 opdracht 13

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
+var aantalRijenRaster = 12;
+var aantalKolommenRaster = 18;
+var celGrootte;
+
 var animatie = [];
-var aantalBeeldjes = 3;
-var nummer = 0;
+var aantalBeeldjes = 6;
+var nummer = 3;
+
+var frame;
+var xJos = 400;
+var yJos = 300;
 
 function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
   for (var b = 0;b < aantalBeeldjes;b++) {
-    nieuw_beeldje = loadImage("images/sprites/Jos_losse_beeldjes/Jos-" + b +".png");
-    animatie.push(nieuw_beeldje);
+    frame = loadImage("images/sprites/Jos100px/Jos_" + b + ".png");
+    animatie.push(frame);
   }
 }
 
 function setup() {
-  canvas = createCanvas(460,460);
+  canvas = createCanvas(900,600);
   canvas.parent();
-  noStroke();
-  frameRate(2);
-  textFont("Georgia");
-  textSize(18);
+  frameRate(10);
+  celGrootte = width / aantalKolommenRaster;
 }
 
 function draw() {
-  background('lavender');
-  image(animatie[nummer],80,160,300,300);
-  nummer++;
+  background(brug);
+  tekenRaster();
 
-  if (nummer == aantalBeeldjes) {
-    nummer = 0;
+  if (keyIsDown(LEFT_ARROW)) {
+    xJos -= celGrootte;
+    nummer = 2;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    xJos += celGrootte;
+    nummer = 1;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    yJos -= celGrootte;
+    nummer = 4;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yJos += celGrootte;
+    nummer = 5;
   }
   
-  text("frameCount=" + frameCount,5,20);
-  text("nummer=" + nummer,5,40);
+  xJos = constrain(xJos,0,width - celGrootte);
+  yJos = constrain(yJos,0,height - celGrootte);
+  
+  image(animatie[nummer],xJos,yJos,celGrootte,celGrootte);
+}
+
+function tekenRaster() {
+  push();
+  noFill();
+  stroke('grey');
+  for (var rij = 0;rij < aantalRijenRaster;rij++) {
+    for (var kolom = 0;kolom < aantalKolommenRaster;kolom++) {
+      rect(kolom*celGrootte,rij*celGrootte,celGrootte,celGrootte);
+    }
+  }
+  pop();
 }
