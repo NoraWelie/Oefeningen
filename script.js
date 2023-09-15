48ba32affd937c5f77875b123840acec7ccefbed
NoraWelie <113632553+NoraWelie@users.noreply.github.com>
2023-09-15 12:20:20 +0000

    2.6 opdracht 20

--- script.js
+++ script.js
@@ -19,18 +19,46 @@ var raster = {
   }
 }
 
-var animatie = [];
-var aantalBeeldjes = 6;
-var nummer = 3;
-var frame;
-var xJos = 400;
-var yJos = 300;
+var jos = {
+  x: 400,
+  y: 300,
+  animatie: [],
+  aantalFrames: 6,
+  frameNummer: 3,
+  stapGrootte: null,
+
+  beweeg() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= raster.celGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+    
+    this.x = constrain(this.x,0,width - raster.celGrootte);
+    this.y = constrain(this.y,0,height - raster.celGrootte);
+  },
+  
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
 
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
-  for (var b = 0;b < aantalBeeldjes;b++) {
-    frame = loadImage("images/sprites/Jos100px/Jos_"+b+".png");
-    animatie.push(frame);
+  for (var b = 0;b < jos.aantalFrames;b++) {
+    frame = loadImage("images/sprites/Jos100px/Jos_" + b + ".png");
+    jos.animatie.push(frame);
   }
 }
 
@@ -39,31 +67,12 @@ function setup() {
   canvas.parent();
   frameRate(10);
   raster.berekenCelGrootte();
+  jos.stapGrootte = 2 * raster.celGrootte;
 }
 
 function draw() {
   background(brug);
   raster.teken();
-
-  if (keyIsDown(LEFT_ARROW)) {
-    xJos -= raster.celGrootte;
-    nummer = 2;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    xJos += raster.celGrootte;
-    nummer = 1;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    yJos -= raster.celGrootte;
-    nummer = 4;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    yJos += raster.celGrootte;
-    nummer = 5;
-  }
-  
-  xJos = constrain(xJos,0,width - raster.celGrootte);
-  yJos = constrain(yJos,0,height - raster.celGrootte);
-  
-  image(animatie[nummer],xJos,yJos,raster.celGrootte,raster.celGrootte);
+  jos.beweeg();
+  jos.toon();
 }
